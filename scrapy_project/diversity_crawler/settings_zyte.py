4eb47f6fbbf7134bb2cf3ce99062a2d7e4cf33f2
Maria Sebastian <maria-rosario.sebastian@edu.dsti.institute>
2022-02-02 15:30:25 +0100

    update unsplash spider codes and proxy middleware settings

--- scrapy_project/diversity_crawler/settings_zyte.py
+++ scrapy_project/diversity_crawler/settings_zyte.py
@@ -12,7 +12,7 @@ BOT_NAME = 'diversity'
 SPIDER_MODULES = ['diversity_crawler.spiders']
 NEWSPIDER_MODULE = 'diversity_crawler.spiders'
 
-CLOSESPIDER_PAGECOUNT = 3
+#CLOSESPIDER_PAGECOUNT = 3
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'
--- scrapy_project/diversity_crawler/spiders/unsplash.py
+++ scrapy_project/diversity_crawler/spiders/unsplash.py
@@ -1,44 +1,57 @@
 import scrapy
-from scrapy.loader import ItemLoader
-from itemloaders.processors import MapCompose
+from scrapy.utils.log import configure_logging
 import urllib
 from ..items import DiversityCrawlerItem
 import json
-import os, time
+import time
+import logging
 
 class UnsplashSpider(scrapy.Spider):
     name = 'unsplash'
     allowed_domains = ['unsplash.com']
     start_urls = ['http://unsplash.com/']
     site = 'unsplash'
-    page_cnt=100
 
     custom_settings = {
         'ITEM_PIPELINES': {
             'diversity_crawler.pipelines.DataWriterPipeline': 300
-        }
+        },
+        'LOG_STDOUT':True
     }
 
-    params = {'query': 'human faces',
+    configure_logging(install_root_handler=False)
+
+    logging.basicConfig(
+        filename='logs/unsplash_log.txt',
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    params = {'query': 'people',
 			'per_page': 20,
             'page': 1,
             'xp': 'search-multi-word'
 		}
     
     folder=time.strftime('%Y%m%d%H%M%S',time.localtime())
-    
-    start_urls = []
-    #for i in range(0,params['page[size]']):
-    for i in range(1,page_cnt):
-        start_urls.append('https://unsplash.com/napi/search/photos?'+ urllib.parse.urlencode(params))
-        params['page'] = i
+    start_urls = ['https://unsplash.com/napi/search/photos?'+ urllib.parse.urlencode(params)]
+
+    def parse(self, response):
+        base_url = 'https://unsplash.com/napi/search/photos?'
+        data = json.loads(response.body)
+        page_cnt = data['total_pages']
+        print("number of pages: ", page_cnt)
+        logging.info("number of pages: ", page_cnt)
+        for page in range(1, page_cnt):
+            self.params['page'] = page
+            yield scrapy.Request(base_url + urllib.parse.urlencode(self.params), dont_filter=True, callback=self.parse_page)
 
     #def __init__(self):
         #os.makedirs('images/'+self.folder)
 
-    def parse(self, response):
+    def parse_page(self, response):
         response =json.loads(response.body)
-        #self.page_cnt = response['meta']['pagination']['totalPages'] - 11873,
         for result in response['results']:
             item = DiversityCrawlerItem()
             item['folder'] = self.folder
@@ -47,8 +60,13 @@ class UnsplashSpider(scrapy.Spider):
             item['ethnicity'] =  'NA'#self.params['filter[people_ethnicity]']
             item['img_urls'] =  result['urls']['small']
             item['filename'] =  self.site+'_'+result['id']
-            item['description'] =  result['alt_description'].replace(',','')
+            if result['alt_description'] is not None:
+                item['description'] =  result['alt_description'].replace(',','')
+            else:
+                item['description'] =  'NA'
             item['img_type'] =  'NA'
             if len(item['img_urls']) != 0:
                 urllib.request.urlretrieve(item['img_urls'], 'images/unsplash/'+item['filename']+'.jpg')
             yield item
+
+    
