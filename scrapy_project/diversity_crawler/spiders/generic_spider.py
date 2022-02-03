b72c99f3acdfc26d2e72cb6a1955e57f58bf176f
Maria Sebastian <maria-rosario.sebastian@edu.dsti.institute>
2022-02-03 18:32:57 +0100

    Update on generic_spider for environment variable and config file settings

--- scrapy_project/diversity_crawler/spiders/generic_spider.py
+++ scrapy_project/diversity_crawler/spiders/generic_spider.py
@@ -1,3 +1,13 @@
+'''
+This spider is a generic spider for istockphoto.com, unsplash.com and shutterstock.com
+It takes the JSON data from utils/site_data.json file to parse the web site, query parameters and results path
+It gets the environment variable, HOME_DIR as the project folder
+The utils/crawler.ini contains the configuration file such as which site is going to be scraped
+It takes the JSON response from the web sites, which contain only data (no formatting), for faster download and
+less burden to the web servers
+Images are downloaded together with the metadata
+The images are saved in the images folder, the csv in csv folder, and the log files in the logs folder
+'''
 import scrapy
 from scrapy.utils.log import configure_logging
 import logging
@@ -21,11 +31,13 @@ class GenericSpiderSpider(scrapy.Spider):
     site_data = []
     params = []
     
-    folder=time.strftime('%Y%m%d%H%M%S',time.localtime())
-    start_urls = []
-    #start_urls = ['https://unsplash.com/napi/search/photos?'+ urllib.parse.urlencode(params)]
+    
+    images_folder=''
 
+    start_urls = []
+    
     def __init__(self):
+        HOME_DIR = os.getenv('CRAWLER_HOME')
         config = configparser.ConfigParser()
         config.read('utils/crawler.ini')
         
@@ -34,14 +46,27 @@ class GenericSpiderSpider(scrapy.Spider):
 
         configure_logging(install_root_handler=False)
 
-        os.makedirs('images/'+self.folder)
-        if os.path.isdir('logs'):
+        #create images folder, if not existing
+        if os.path.isdir(HOME_DIR+config['APP']['IMAGES_FOLDER']):
             pass
         else:
-            os.makedirs('logs')
+            os.makedirs(HOME_DIR+config['APP']['IMAGES_FOLDER'])
+
+        timestamp = time.strftime('%Y%m%d%H%M%S',time.localtime())
+        self.images_folder = HOME_DIR+config['APP']['IMAGES_FOLDER']+'/'+self.site+'_'+timestamp
+        
+        #create the folder for the images to be downloaded
+        os.makedirs(self.images_folder)
+        
+        #create logs folder, if not existing
+        if os.path.isdir(HOME_DIR+config['APP']['LOG_FOLDER']):
+            pass
+        else:
+            os.makedirs(HOME_DIR+config['APP']['LOG_FOLDER'])
+        
 
         logging.basicConfig(
-            filename='logs/'+self.site+'_log.txt',
+            filename=HOME_DIR+config['APP']['LOG_FOLDER']+'/'+self.site+'_log.txt',
             format='%(asctime)s %(levelname)-8s %(message)s',
             level=logging.INFO,
             datefmt='%Y-%m-%d %H:%M:%S'
@@ -109,5 +134,5 @@ class GenericSpiderSpider(scrapy.Spider):
                     item['img_type'] =  result[self.site_data['results']['img_type_1']][self.site_data['results']['img_type_2']]
                 else:
                     item['img_type'] =  result[self.site_data['results']][self.site_data['img_type']]
-                urllib.request.urlretrieve(item['img_urls'], 'images/'+item['filename']+'.jpg')
+                urllib.request.urlretrieve(item['img_urls'], self.images_folder+'/'+item['filename']+'.jpg')
                 yield item
