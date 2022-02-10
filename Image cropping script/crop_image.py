32fd99e91aa48dc74a89099cde135edc35688820
Allan Kouidri <87222273+allankouidri@users.noreply.github.com>
2022-02-10 17:22:12 +0000

    Update crop_image.py

--- Image cropping script/crop_image.py
+++ Image cropping script/crop_image.py
@@ -1,20 +1,28 @@
 from PIL import Image
 import glob
+import os
+paths = {
+    'IMAGE_TOCROP_PATH': os.path.join('crop-images', 'pre-process'), 
+    'CROPPED_PATH': os.path.join('crop-images', 'post-process'), 
+ }
 
-
+#creating our folders 
+for path in paths.values():
+    if not os.path.exists(path):
+            !mkdir {path}
+            
 #create a list of the images to crop
-image_list = [i for i in glob.glob('YOUR_DIRECTORY\\*.jpg')]
+image_list = [i for i in glob.glob(os.path.join('crop-images', 'pre-process','*jpg'))]
 
 # resized/cropped images to list
 for i in image_list:
     '''
-    Crop the bottom 15 pixels for the images. 
+    Crop the bottom 20 pixels for the images. 
     Images will be overriden
     '''
     cropped_images = Image.open(i)
     cropped_images = cropped_images.crop((0, 0, cropped_images.width, cropped_images.height - 20))
     cropped_images.save(i)
-
 
 
 
